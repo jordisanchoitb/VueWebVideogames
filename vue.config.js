242a15f20999fbb77d50257e185c1a575b890e69
jordisanchoitb <146190178+jordisanchoitb@users.noreply.github.com>
2024-05-08 17:42:10 +0000

    solucionado bug

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  devServer: {
-    proxy: {
-      '/ws': {
-        target: 'wss://localhost:3000', // Cambia esto a la URL de tu servidor WebSocket seguro
-        ws: true,
-        changeOrigin: true
-      }
-    }
-  },
   transpileDependencies: true
 })
